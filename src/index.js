70e13fe4b1ca76d768ef77f0fa6e73eebb4486ad
oriravid <oriravid@att.net>
2021-02-01 19:27:00 -0800

    Google Analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import HttpsRedirect from "react-https-redirect";
+import ReactGA, { initialize } from "react-ga";
 //int - css
 import "./css/resets.css";
 import "./css/styles.scss";
@@ -10,6 +11,12 @@ import Nav from "./js/components/nav";
 import Home from "./js/components/home";
 import Projects from "./js/components/projects";
 import About from "./js/components/about";
+
+function initializeReactGA() {
+    ReactGA.initialize("UA-50867458-1");
+    ReactGA.pageview("/");
+}
+initializeReactGA();
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
